ffd0f5c46ad9279b21f34687953a36a991e62ff9
zengdehui <88344512@qq.com>
2019-09-16 10:31:48 +0800

    修改元素信息表 编辑和删除方式

--- src/components/project/page/elementTable.vue
+++ src/components/project/page/elementTable.vue
@@ -4,58 +4,101 @@
     <el-table
       ref="multipleTable"
       :data="elesData.filter(data => !search || data.title.toLowerCase().includes(search.toLowerCase()))"
-      tooltip-effect="dark"
+      fit highlight-current-row
       style="width: 100%"
-      >
+    >
       <el-table-column
-        prop="id"
         label="ID"
-      width="60">
+        width="60">
+        <template slot-scope="scope">
+          <span>{{ scope.row.id }}</span>
+        </template>
       </el-table-column>
-      <el-table-column
-        prop="title"
-        label="元素名称">
+
+      <el-table-column width="150px" label="元素名称">
+        <template slot-scope="{row}">
+          <template v-if="row.edit">
+            <el-input v-model="row.title" class="edit-input" size="small"/>
+          </template>
+          <span v-else>{{ row.title }}</span>
+        </template>
       </el-table-column>
-      <el-table-column
-        prop="type"
-        label="查找方式">
+      <el-table-column width="150px" label="查找方式">
+        <template slot-scope="{row}">
+          <template v-if="row.edit">
+            <el-input v-model="row.type" class="edit-input" size="small"/>
+          </template>
+          <span v-else>{{ row.type }}</span>
+        </template>
       </el-table-column>
-            <el-table-column
-        prop="loc"
-        label="元素位置">
+
+      <el-table-column width="200px" label="元素位置">
+        <template slot-scope="{row}">
+          <template v-if="row.edit">
+            <el-input v-model="row.loc" class="edit-input" size="small"/>
+          </template>
+          <span v-else>{{ row.loc }}</span>
+        </template>
       </el-table-column>
       <el-table-column
         prop="update_datetime"
         label="更新时间">
       </el-table-column>
-
-      <el-table-column
-        align="right">
+      <el-table-column align="center" label="操作" width="300">
         <template slot="header" slot-scope="scope">
-          <el-button type="primary" @click="operationDialog(2,'')">新增<i class="el-icon-plus el-icon--right"></i></el-button>
+          <el-button type="primary" @click="addForm=true">新增<i class="el-icon-plus el-icon--right"></i>
+          </el-button>
           <el-input
             v-model="search"
             size="mini"
             placeholder="输入元素名称搜索"/>
         </template>
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            @click="operationDialog(1,scope.row)">编辑
-          </el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="operationDialog(3,scope.row)">删除
-          </el-button>
+        <template slot-scope="{row}">
+          <div v-if="row.edit">
+            <el-button
+              type="success"
+              size="small"
+              icon="el-icon-circle-check-outline"
+              @click="confirmEdit(row)"
+            >
+              确定
+            </el-button>
+            <el-button
+              class="cancel-btn"
+              size="small"
+              icon="el-icon-refresh"
+              type="warning"
+              @click="cancelEdit(row)"
+            >
+              取消编辑
+            </el-button>
+          </div>
+
+          <div v-else>
+            <el-button
+              type="primary"
+              size="small"
+              icon="el-icon-edit"
+              @click="row.edit=!row.edit"
+            >
+              编辑
+            </el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="el-icon-edit"
+              @click="deleteRow(row.id)"
+            >
+              删除
+            </el-button>
+          </div>
         </template>
       </el-table-column>
-
     </el-table>
 
     <!--    添加-->
 
-    <el-dialog title="添加" :visible.sync="addFormVisible">
+    <el-dialog title="添加" :visible.sync="addForm">
       <el-form :model="form">
         <el-form-item label="元素标题：" :label-width="formLabelWidth">
           <el-input v-model="form.title" autocomplete="off"></el-input>
@@ -68,43 +111,9 @@
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="addFormVisible = false">取 消</el-button>
+        <el-button @click="addForm = false">取 消</el-button>
         <el-button type="primary" @click="addElement">确 定</el-button>
       </div>
-    </el-dialog>
-
-    <!--    编辑-->
-
-    <el-dialog title="'编辑" :visible.sync="editFormVisible">
-      <el-form :model="form">
-        <el-form-item label="元素标题：" :label-width="formLabelWidth">
-          <el-input v-model="form.title" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="查找方式：" :label-width="formLabelWidth">
-          <el-input v-model="form.type" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="元素位置：" :label-width="formLabelWidth">
-          <el-input v-model="form.loc" autocomplete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="editFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="editElement">确 定</el-button>
-      </div>
-    </el-dialog>
-
-    <!--      删除-->
-
-    <el-dialog
-      title="删除"
-      :visible.sync="deleteDialogVisible"
-      width="30%"
-      center>
-      <span>{{eleTitle}}</span>
-      <span slot="footer" class="dialog-footer">
-    <el-button @click="deleteDialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="delElements">确 定</el-button>
-  </span>
     </el-dialog>
   </div>
 
@@ -120,11 +129,9 @@
         search: '',
         eleTitle: '',
         elesData: [],
-        currentElementId: '',
-        addFormVisible: false,
+        addForm: false,
         editFormVisible: false,
         deleteDialogVisible: false,
-        dialogTitle: '',
         form: {
           title: '',
           type: '',
@@ -135,38 +142,11 @@
     },
 
     methods: {
-      operationDialog(ope, row) {
-        switch (ope) {
-          case 1:
-            // 编辑
-            this.editFormVisible = true
-            this.currentElementId = row.id
-            this.form.title = row.title
-            this.form.type = row.type
-            this.form.loc = row.loc
-
-
-            break
-          case 2:
-            // 添加
-            this.addFormVisible = true
-            this.form.title = ''
-            this.form.type = ''
-            this.form.loc = ''
-            break
-          case 3:
-            // 删除
-            this.deleteDialogVisible = true
-            this.currentElementId = row.id
-            this.eleTitle = '确定删除：' + row.title
-            break
-        }
-      },
       addElement() {
 
         postElement(this.$route.params.id, this.$route.params.page_id, this.form).then(res => {
           if (res.status == 1) {
-            this.addFormVisible = false
+            this.addForm = false
             this.$alert(res.msg)
             this.getElementData()
             this.search = ''
@@ -175,28 +155,56 @@
           }
         })
       },
-      editElement() {
-        editElement(this.$route.params.id, this.$route.params.page_id, this.currentElementId, this.form).then(res => {
+      confirmEdit(row) {
+        row.edit = false
+        row.originalTitle = row.title
+        row.originalLoc = row.loc
+        row.originalType = row.type
+        editElement(this.$route.params.id, this.$route.params.page_id, row.id, row).then(res => {
           if (res.status == 1) {
-            this.editFormVisible = false
-            this.$alert(res.msg)
+            this.$message({
+              message: '编辑成功',
+              type: 'success'
+            })
             this.getElementData()
-            this.search = ''
           } else {
-            this.$alert(res.msg)
+            this.$message({
+              message: res.msg,
+              type: 'error'
+            })
           }
         })
       },
-      delElements() {
-        deleteElement(this.$route.params.id, this.$route.params.page_id, this.currentElementId).then(res => {
-          if (res.status == 1) {
-            this.deleteDialogVisible = false
-            this.$alert(res.msg)
-            this.getElementData()
-            this.search = ''
-          } else {
-            this.$alert(res.msg)
+      cancelEdit(row) {
+        row.title = row.originalTitle
+        row.loc = row.originalLoc
+        row.type = row.originalType
+        row.edit = false
+
+        this.$message({
+          message: '放弃编辑',
+          type: 'warning'
+        })
+      },
+      deleteRow(rowId) {
+        this.$confirm('确定删除?', '删除', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+        }).then(() => {
+            deleteElement(this.$route.params.id, this.$route.params.page_id, rowId).then(res => {
+              if (res.status == 1) {
+                this.getElementData()
+                this.search = ''
+                this.$message({
+                  type: 'info',
+                  message: res.msg
+                });
+              } else {
+                this.$alert(res.msg)
+              }
+            })
           }
+        ).catch(() => {
         })
       },
       getElementData() {
@@ -204,7 +212,17 @@
         let pa_id = this.$route.params.page_id
         getElementList(project_id, pa_id).then(
           res => {
-            this.elesData = res.data.data_list;
+            if (res.data.data_list.length > 0) {
+              this.elesData = res.data.data_list.map(v => {
+                this.$set(v, 'edit', false) // https://vuejs.org/v2/guide/reactivity.html
+                v.originalTitle = v.title //  will be used when user click the cancel botton
+                v.originalType = v.type //  will be used when user click the cancel botton
+                v.originalLoc = v.loc //  will be used when user click the cancel botton
+                return v
+              })
+            } else {
+              this.elesData = res.data.data_list
+            }
           },
           err => {
             console.log(err);
